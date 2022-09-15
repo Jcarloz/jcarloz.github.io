60b0b430492fb901bc5b92d8ff66dfec61a0e7ab
josecarloscarri <Jcarlozcarrillo@gmail.com>
2022-09-15 04:49:31 -0400

    coding01pos

---
+++ config/global.php
@@ -0,0 +1,21 @@
+<?php 
+//ip de la pc servidor base de datos
+define("DB_HOST", "localhost");
+
+// nombre de la base de datos
+define("DB_NAME", "id19571916_coding01");
+
+
+//nombre de usuario de base de datos
+define("DB_USERNAME", "id19571916_root");
+
+//conraseña del usuario de base de datos
+define("DB_PASSWORD", "|i@\i!9ObE87zLE$");
+
+//codificacion de caracteres
+define("DB_ENCODE", "utf8");
+
+//nombre del proyecto
+define("PRO_NOMBRE", "coding01");
+ 
+ ?>
